5d5681cdcef225689f926fd3e9aa02bbe19d571d
minjin kim <sajajin115@gmail.com>
2024-09-30 10:08:09 +0900

    mvvm + repos directory architecture update

---
+++ app/src/main/java/com/example/myapplication/ui/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
