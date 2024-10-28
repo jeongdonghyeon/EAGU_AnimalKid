2a348ff3b064414fd934e0ef065e2ed93a0471f5
Kenneth <bethe8708@naver.net>
2024-10-28 19:34:49 +0900

    CalendarViewModel Modify

--- app/src/main/java/com/example/myapplication/ui/viewmodel/CalendarViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/viewmodel/CalendarViewModel.kt
@@ -2,13 +2,13 @@ package com.example.myapplication.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.myapplication.data.model.entity.CalendarEntity
+import com.example.myapplication.data.model.entity.CalendarEvent
 import com.example.myapplication.data.repository.CalendarRepository
 import kotlinx.coroutines.launch
 
  class CalendarViewModel(private val repository: CalendarRepository) : ViewModel(){
 
-        fun insert(event: CalendarEntity.CalendarEvent)=viewModelScope.launch {
+        fun insert(event:CalendarEvent)=viewModelScope.launch {
             repository.insert(event)
         }
 
@@ -16,11 +16,11 @@ import kotlinx.coroutines.launch
             repository.getAll()
         }
 
-        fun update(event: CalendarEntity.CalendarEvent)=viewModelScope.launch {
+        fun update(event: CalendarEvent)=viewModelScope.launch {
             repository.update(event)
         }
 
-        fun delete(event: CalendarEntity.CalendarEvent)=viewModelScope.launch {
+        fun delete(event: CalendarEvent)=viewModelScope.launch {
             repository.delete(event)
         }
     }
