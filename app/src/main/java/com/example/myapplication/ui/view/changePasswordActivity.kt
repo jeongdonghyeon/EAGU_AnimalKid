bba6fab7bc5573d416f4a5e26e9da35f29447bca
minjin kim <sajajin115@gmail.com>
2024-10-31 20:52:41 +0900

    add btn

--- app/src/main/java/com/example/myapplication/ui/view/changePasswordActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/view/changePasswordActivity.kt
@@ -1,22 +1,69 @@
 package com.example.myapplication.ui.view
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.databinding.ActivityLoginBinding
+import com.example.myapplication.databinding.FindIdBinding
+import com.example.myapplication.databinding.FindPwBinding
 import com.example.myapplication.ui.viewmodel.AuthViewModel
+import kotlinx.coroutines.launch
 
 class changePasswordActivity : AppCompatActivity(){
 
-    private lateinit var  binding: ActivityLoginBinding
-    private lateinit var authViewModel : AuthViewModel
+    private lateinit var  binding: FindPwBinding
+    private lateinit var authViewModel: AuthViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FindPwBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupToolbar()
+        setupListeners()
+    }
+    // 툴바에 있는 뒤로가기 버튼을 클릭했을 때
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+        }
+        binding.back.setOnClickListener{
+            finish()
+        }
+    }
+    // 핸드폰  자체 기능인 뒤로가기 했을 때
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    private fun setupListeners() {
+        binding.requestNumber.setOnClickListener{
+            lifecycleScope.launch {
+                emailVerification()
+            }
+        }
+        binding.rerequestNumber.setOnClickListener{
+            lifecycleScope.launch {
+                emailVerification()
+            }
+        }
+        binding.authenticationButton.setOnClickListener{
+            lifecycleScope.launch {
+                verifyCode()
+            }
+        }
+    }
 
 
     private suspend fun emailVerification(){
-        val email = binding.emailEditText.text.toString()
-        val username = binding.emailEditText.text.toString()
+        val email = binding.enterId.text.toString()
+        val username = binding.enterEmail.text.toString()
         authViewModel.changePassword(email,username)
     }
     private suspend fun verifyCode(){
-        val inputCode = binding.emailEditText.text.toString()
+        val inputCode = binding.enterAuthentication.text.toString()
         authViewModel.verifyCode(inputCode)
     }
 
--- app/src/main/java/com/example/myapplication/ui/view/registerActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/view/registerActivity.kt
@@ -1,26 +1,60 @@
 package com.example.myapplication.ui.view
 
+import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.example.myapplication.data.model.DTO.UserDTO
 import com.example.myapplication.databinding.ActivityLoginBinding
+import com.example.myapplication.databinding.ActivityRegisterBinding
+import com.example.myapplication.databinding.FindIdBinding
 import com.example.myapplication.ui.viewmodel.AuthViewModel
 import com.example.myapplication.ui.viewmodel.Session.UserSessionManager
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import java.util.UUID
 
 class registerActivity : AppCompatActivity(){
 
-    private lateinit var  binding: ActivityLoginBinding
+    private lateinit var  binding: ActivityRegisterBinding
     private lateinit var authViewModel : AuthViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupToolbar()
+        setupListeners()
+    }
+    // 툴바에 있는 뒤로가기 버튼을 클릭했을 때
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.apply {
+            setDisplayShowTitleEnabled(false)
+        }
+        binding.backBtn.setOnClickListener{
+            finish()
+        }
+    }
+    // 핸드폰  자체 기능인 뒤로가기 했을 때
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+    private fun setupListeners(){
+        binding.LoginButton.setOnClickListener{
+            lifecycleScope.launch {
+                registerUser()
+            }
+        }
+    }
 
     private suspend fun registerUser(){
-        val email = binding.emailEditText.text.toString()
-        val username = binding.emailEditText.text.toString()
-        val password1 = binding.passwordEditText.text.toString()
-        val password2 = binding.passwordEditText.text.toString()
+        val email = binding.IDedittext.text.toString()
+        val username = binding.Emailedittext.text.toString()
+        val password1 = binding.PWedittext.text.toString()
+        val password2 = binding.PWcheckedittext.text.toString()
 
         if(password1 == password2){
 
